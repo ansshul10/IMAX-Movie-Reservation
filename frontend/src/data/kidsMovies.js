35c857884d3d90f1c927aae08d1f7520f668befb
ansshul10 <ansshul10@gmail.com>
2025-03-08 20:18:15 +0530

    Update kidsMovies.js

--- frontend/src/data/kidsMovies.js
+++ frontend/src/data/kidsMovies.js
@@ -1,95 +1,124 @@
 const kidsMovies = [
-    {
-      id: 1,
-      title: "Toy Story",
-      year: 1995,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 8.3,
-      image: "https://wallpapers.com/images/hd/toy-story-portrait-art-ubghxa4vbl58xeam.jpg",
-      trailer: "v-PjgYDrg70", // YouTube video ID for the trailer
-    },
-    {
-      id: 2,
-      title: "The Lion King",
-      year: 1994,
-      genre: "Animation, Adventure, Drama",
-      imdb: 8.5,
-      image: "https://m.media-amazon.com/images/I/7148fvlOyOL.jpg",
-      trailer: "4sj1FwmYbpI", // YouTube video ID for the trailer
-    },
-    {
-      id: 3,
-      title: "Frozen",
-      year: 2013,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 7.4,
-      image: "https://image.tmdb.org/t/p/original/50Xpfgtuk6OrTlfFYuktW0IJapW.jpg",
-      trailer: "TbqKk2I5WlI", // YouTube video ID for the trailer
-    },
-    {
-      id: 4,
-      title: "Shrek",
-      year: 2001,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 7.9,
-      image: "https://image.tmdb.org/t/p/original/GRcW6yGfRckR7dAPYNCTXwt8Na.jpg",
-      trailer: "4p8tU69FyrM", // YouTube video ID for the trailer
-    },
-    {
-      id: 5,
-      title: "Kung Fu Panda",
-      year: 2008,
-      genre: "Animation, Action, Adventure",
-      imdb: 7.6,
-      image: "https://image.tmdb.org/t/p/original/oMkr9vKlJ5Qfj2o5zV2BcaEZcQu.jpg",
-      trailer: "4qpm3b06R3k", // YouTube video ID for the trailer
-    },
-    {
-      id: 6,
-      title: "Despicable Me",
-      year: 2010,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 7.6,
-      image: "https://rukminim2.flixcart.com/image/850/1000/j752nww0/poster/y/a/z/large-paradise-minions-despicable-me-2-on-fine-art-paper-hd-original-imaerxz9aeq2h74g.jpeg?q=20&crop=false",
-      trailer: "6Pbt3lTYcQk", // YouTube video ID for the trailer
-    },
-    {
-      id: 7,
-      title: "The Incredibles",
-      year: 2004,
-      genre: "Animation, Action, Adventure",
-      imdb: 8.0,
-      image: "https://wallpapercat.com/w/full/0/6/b/166083-1080x1920-samsung-1080p-the-incredibles-background-photo.jpg",
-      trailer: "iIsKjz1Ydd8", // YouTube video ID for the trailer
-    },
-    {
-      id: 8,
-      title: "Ice Age",
-      year: 2002,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 6.8,
-      image: "https://wallpapercat.com/w/full/7/a/f/2116602-1400x2100-mobile-hd-ice-age-background-image.jpg",
-      trailer: "1RUGI6lM7I4", // YouTube video ID for the trailer
-    },
-    {
-      id: 9,
-      title: "Beauty and the Beast",
-      year: 1991,
-      genre: "Animation, Family, Fantasy",
-      imdb: 8.0,
-      image: "https://image.tmdb.org/t/p/original/6xXRbGKD9ir5tEVn270fjLUzl0G.jpg",
-      trailer: "k9REbzAnv0w", // YouTube video ID for the trailer
-    },
-    {
-      id: 10,
-      title: "Aladdin",
-      year: 1992,
-      genre: "Animation, Adventure, Comedy",
-      imdb: 8.0,
-      image: "https://w0.peakpx.com/wallpaper/864/540/HD-wallpaper-aladdin-2019-movie-aladdin-movie-aladdin-2019-movies-movies-thumbnail.jpg",
-      trailer: "gDz2nH7e3A0", // YouTube video ID for the trailer
-    },
-  ];
-  
-  export default kidsMovies;
-  
+  {
+    id: 1,
+    title: "The Lion King",
+    year: 1994,
+    genre: "Animation, Adventure, Drama",
+    imdb: 8.5,
+    image: "https://m.media-amazon.com/images/I/7148fvlOyOL.jpg",
+    trailer: "lFzVJEksoDY", // Official Hindi trailer (Disney India)
+    cast: ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"],
+    runtime: "1h 28m",
+    description: "A young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
+  },
+  {
+    id: 2,
+    title: "Kung Fu Panda",
+    year: 2008,
+    genre: "Animation, Action, Adventure",
+    imdb: 7.6,
+    image: "https://m.media-amazon.com/images/M/MV5BODJkZTZhMWItMDI3Yy00ZWZlLTk4NjQtOTI1ZjU5NjBjZTVjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
+    trailer: "oH1T9Bsp968", // Official Hindi trailer (Universal Pictures India)
+    cast: ["Jack Black", "Dustin Hoffman", "Angelina Jolie"],
+    runtime: "1h 32m",
+    description: "A clumsy panda becomes an unlikely martial arts hero to save his valley from a fierce enemy.",
+  },
+  {
+    id: 3,
+    title: "Despicable Me",
+    year: 2010,
+    genre: "Animation, Adventure, Comedy",
+    imdb: 7.6,
+    image: "https://m.media-amazon.com/images/I/81CwVk8DzQL._SY741_.jpg",
+    trailer: "sukIr0zDoxQ", // Official Hindi trailer (Universal Pictures India)
+    cast: ["Steve Carell", "Jason Segel", "Russell Brand"],
+    runtime: "1h 35m",
+    description: "A supervillain adopts three orphan girls and discovers the joys of fatherhood while planning a heist.",
+  },
+  {
+    id: 4,
+    title: "Ice Age",
+    year: 2002,
+    genre: "Animation, Adventure, Comedy",
+    imdb: 7.5,
+    image: "https://i.ebayimg.com/images/g/HRAAAMXQUY1Q9e6~/s-l500.jpg",
+    trailer: "pXYB3pS6mA8", // Official Hindi trailer (20th Century Fox India)
+    cast: ["Ray Romano", "John Leguizamo", "Denis Leary"],
+    runtime: "1h 21m",
+    description: "A group of prehistoric animals bands together to return a human baby to its tribe.",
+  },
+  {
+    id: 5,
+    title: "Finding Nemo",
+    year: 2003,
+    genre: "Animation, Adventure, Comedy",
+    imdb: 8.2,
+    image: "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
+    trailer: "SPHAe6a4Ku4", // Official Hindi trailer (Disney India)
+    cast: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+    runtime: "1h 40m",
+    description: "A clownfish father searches the ocean to find his lost son with the help of a forgetful friend.",
+  },
+  {
+    id: 6,
+    title: "Madagascar",
+    year: 2005,
+    genre: "Animation, Adventure, Comedy",
+    imdb: 6.9,
+    image: "https://m.media-amazon.com/images/M/MV5BN2I5YzFlYWEtZjRhNy00ZmQzLWJhNTktZGIwYjFjODdmNDgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+    trailer: "885YLDV2xEY", // Official Hindi trailer (DreamWorks India)
+    cast: ["Ben Stiller", "Chris Rock", "David Schwimmer"],
+    runtime: "1h 26m",
+    description: "Zoo animals escape to the wild and learn to survive on a remote island.",
+  },
+  {
+    id: 7,
+    title: "Tangled",
+    year: 2010,
+    genre: "Animation, Adventure, Comedy",
+    imdb: 7.7,
+    image: "https://m.media-amazon.com/images/M/MV5BMTAxNDYxMjg0MjNeQTJeQWpwZ15BbWU3MDcyNTk2OTM@._V1_.jpg",
+    trailer: "qUq2sK1KZ1M", // Official Hindi trailer (Disney India)
+    cast: ["Mandy Moore", "Zachary Levi", "Donna Murphy"],
+    runtime: "1h 40m",
+    description: "A long-haired princess escapes her tower with a charming thief to discover the world.",
+  },
+  {
+    id: 8,
+    title: "The Jungle Book",
+    year: 2016,
+    genre: "Adventure, Drama, Family",
+    imdb: 7.4,
+    image: "https://m.media-amazon.com/images/M/MV5BMTc3NTUzNTI4MV5BMl5BanBnXkFtZTgwNjU0NjU5NzE@._V1_.jpg",
+    trailer: "5mkm22yOQcY", // Official Hindi trailer (Disney India)
+    cast: ["Neel Sethi", "Bill Murray", "Ben Kingsley"],
+    runtime: "1h 46m",
+    description: "A boy raised by wolves must face a fierce tiger and discover his true destiny.",
+  },
+  {
+    id: 9,
+    title: "Cars",
+    year: 2006,
+    genre: "Animation, Comedy, Family",
+    imdb: 7.2,
+    image: "https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_.jpg",
+    trailer: "SBwYQrim2cM", // Official Hindi trailer (Disney India)
+    cast: ["Owen Wilson", "Paul Newman", "Bonnie Hunt"],
+    runtime: "1h 57m",
+    description: "A hotshot racecar learns the value of friendship and humility in a small town.",
+  },
+  {
+    id: 10,
+    title: "Hotel Transylvania",
+    year: 2012,
+    genre: "Animation, Comedy, Family",
+    imdb: 7.0,
+    image: "https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_.jpg",
+    trailer: "zRjDhEXh9kQ", // Official Hindi trailer (Sony Pictures India)
+    cast: ["Adam Sandler", "Andy Samberg", "Selena Gomez"],
+    runtime: "1h 31m",
+    description: "Dracula runs a hotel for monsters, but chaos ensues when a human arrives.",
+  },
+];
+
+export default kidsMovies;
