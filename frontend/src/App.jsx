33f6b676e582bebd7dfd869aacfab41718364a9e
ansshul10 <ansshul10@gmail.com>
2025-03-08 20:22:02 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react"; // Added useState
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
 import Profile from "./pages/Profile";
-import SeriesList from "./pages/SeriesList";  // Route for Series List page
-import SeriesDetails from "./pages/SeriesDetails";  // Route for Series Details page
+import SeriesList from "./pages/SeriesList";
+import SeriesDetails from "./pages/SeriesDetails";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import Kids from "./pages/Kids";
@@ -16,23 +16,34 @@ import BookingPage from "./pages/BookingPage";
 import BookingForm from "./pages/BookingForm";
 import Confirmation from "./pages/Confirmation";
 
-
 const App = () => {
+  const location = useLocation();
+  const [isMuted, setIsMuted] = useState(true); // Added state for audio control
+
+  // Hide Navbar on /admin, /movies/:id, /series/:id, /kids/:id, /BookingPage, /BookingForm, /Confirmation
+  const hideNavbar =
+    location.pathname === "/admin" ||
+    location.pathname.startsWith("/movies/") ||
+    location.pathname.startsWith("/series/") ||
+    location.pathname.startsWith("/kids/") ||
+    location.pathname === "/BookingForm" ||
+    location.pathname === "/Confirmation";
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar isMuted={isMuted} setIsMuted={setIsMuted} />}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home isMuted={isMuted} setIsMuted={setIsMuted} />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
-        
+
         {/* Movies Routes */}
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
-        
+
         {/* Series Routes */}
-        <Route path="/series" element={<SeriesList />} /> 
+        <Route path="/series" element={<SeriesList />} />
         <Route path="/series/:id" element={<SeriesDetails />} />
 
         {/* Kids Routes */}
@@ -43,7 +54,6 @@ const App = () => {
         <Route path="/BookingPage" element={<BookingPage />} />
         <Route path="/BookingForm" element={<BookingForm />} />
         <Route path="/Confirmation" element={<Confirmation />} />
-        
       </Routes>
     </>
   );
