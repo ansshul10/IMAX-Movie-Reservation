ab3b9441af04d9c1c74f76341f9261da9381753a
ansshul10 <ansshul10@gmail.com>
2025-03-09 12:23:13 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,11 +4,13 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  nickname: { type: String, default: "" },  // Nickname field
-  age: { type: Number, default: null },     // Age field
-  profileImage: { type: String, default: "" }, // Profile Image URL
-  balance: { type: Number, default: 0 },    // Balance field (default to 0)
-  pin: { type: String, required: true }     // Pin field (hashed for security)
+  nickname: { type: String, default: "" },
+  age: { type: Number, default: null },
+  profileImage: { type: String, default: "" },
+  balance: { type: Number, default: 0 },
+  pin: { type: String, required: true },
+  resetPasswordToken: { type: String }, // Add reset token field
+  resetPasswordExpires: { type: Date }, // Add expiration field
 });
 
 module.exports = mongoose.model("User", UserSchema);
